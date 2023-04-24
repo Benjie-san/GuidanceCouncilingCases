568b958539544929a5adfbd84bfedb6a9c21842d
Benjiee <haisesasaki56@gmail.com>
2023-04-25 04:40:54 +0800

    last na promise

--- index.php
+++ index.php
@@ -8,33 +8,9 @@
 </head>
 <body>
 
-<?php 
+ <?php 
       include_once 'routes/login.php';
   ?>
-
-  <!-- <?php
-    include_once 'database/db.php';
-    
-    $qry = "SELECT * FROM students";
-    $result = $conn->query($qry);   
-    if(mysqli_num_rows($result) > 0){
-        while($row = $result->fetch_assoc()){
-            $id = $row['ID'];
-            $first = $row["NAME"];
-            $last = $row["AGE"];
-            $email = $row["GRADE"];
-            $number = $row["SECTION"];
-        }
-    }else{
-        echo "no result";
-    } 
-  ?> -->
-<!-- 
-  <h1><?php echo $id?></h1>
-  <h1><?php echo $first?></h1>  
-  <h1><?php echo $last?></h1>
-  <h1><?php echo $email?></h1>
-  <h1><?php echo $number?></h1> -->
 
 </body>
 </html>
