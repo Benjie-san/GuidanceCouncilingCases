24347428ef238e97356415da8cdd30a9ab464410
Benjiee <58848328+Benjie-san@users.noreply.github.com>
2023-04-25 21:54:51 +0800

    Update index.php
    trying

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Career Guidance</title>
-</head>
-<body>
+
   <h1>hello world</h1>
     <?php
       include_once 'database/db.php';
@@ -31,6 +23,3 @@
     <h1><?php echo $last?></h1>
     <h1><?php echo $email?></h1>
     <h1><?php echo $number?></h1>
-
-</body>
-</html>
