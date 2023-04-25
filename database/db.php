7ffa8cadf1b7ef970720248443c43744bc62cc81
Benjiee <haisesasaki56@gmail.com>
2023-04-25 21:28:36 +0800

    maybe it works now

--- database/db.php
+++ database/db.php
@@ -1,10 +1,10 @@
 <?php
-// //credential
-// $dbServername = "localhost";
-// $dbUsername = "root";
-// $dbPassword = "1thousand-7";
-// //database
-// $dbName = "careerguidance";
+//credential
+$dbServername = "sql305.epizy.com";
+$dbUsername = "epiz_34076304";
+$dbPassword = "1thousand-7";
+//database
+$dbName = "epiz_34076304_careerGuidance";
 
-// $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-// ?>
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+?>
--- index.php
+++ index.php
@@ -7,11 +7,30 @@
   <title>Career Guidance</title>
 </head>
 <body>
-
- <?php 
-      //include "routes/login.php";
-      header("location: routes/login.php");
-  ?>
   <h1>hello world</h1>
+    <?php
+      include_once 'database/db.php';
+
+      $qry = "SELECT * FROM students";
+      $result = $conn->query($qry);   
+      if(mysqli_num_rows($result) > 0){
+          while($row = $result->fetch_assoc()){
+              $id = $row['ID'];
+              $first = $row["NAME"];
+              $last = $row["AGE"];
+              $email = $row["GRADE"];
+              $number = $row["SECTION"];
+          }
+      }else{
+          echo "no result";
+      } 
+    ?>
+
+    <h1><?php echo $id?></h1>
+    <h1><?php echo $first?></h1>  
+    <h1><?php echo $last?></h1>
+    <h1><?php echo $email?></h1>
+    <h1><?php echo $number?></h1>
+
 </body>
 </html>
