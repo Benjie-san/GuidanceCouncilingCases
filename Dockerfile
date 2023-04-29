e91ac0a80b4803479171329b50f9fb390f2a9321
Benjiee <haisesasaki56@gmail.com>
2023-04-29 23:00:36 +0800

    docker chnages

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM php:8.0
-COPY ./index.php ./
+FROM php:8.1-apache
+RUN docker-php-ext-install mysqli
+COPY . /var/www/html
 EXPOSE 80
 CMD ["php", "-S", "0.0.0.0:80"]
--- index.php
+++ index.php
@@ -3,9 +3,8 @@
 
 $uri = $_SERVER['REQUEST_URI'];
 
-if($uri === './'){
-  require './controllers/login.php';
+if($uri === '/'){
+  require 'controllers/login.php';
 }
 
-?>
-<h1><?php $uri ?></h1>
+?>.j.y/.//.
