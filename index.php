17902849d4368ffd80734faceb7f51e52c083ebe
Benjiee <haisesasaki56@gmail.com>
2023-04-29 10:24:32 +0800

    .

--- index.php
+++ index.php
@@ -1,36 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Career Guidance</title>
-</head>
-<body>
-  <h1>hello world</h1>
-    <?php
-      include_once 'database/db.php';
+<?php
 
-      $qry = "SELECT * FROM students";
-      $result = $conn->query($qry);   
-      if(mysqli_num_rows($result) > 0){
-          while($row = $result->fetch_assoc()){
-              $id = $row['ID'];
-              $first = $row["NAME"];
-              $last = $row["AGE"];
-              $email = $row["GRADE"];
-              $number = $row["SECTION"];
-          }
-      }else{
-          echo "no result";
-      } 
-    ?>
+$uri = $_SERVER['REQUEST_URI'];
 
-    <h1><?php echo $id?></h1>
-    <h1><?php echo $first?></h1>  
-    <h1><?php echo $last?></h1>
-    <h1><?php echo $email?></h1>
-    <h1><?php echo $number?></h1>
-
-</body>
-</html>
+if($uri === '/'){
+  require 'controllers/login.php';
+}
