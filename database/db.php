913b20be3d62a71b0863acd4abfa0e655e22a9a7
Benjiee <haisesasaki56@gmail.com>
2023-05-02 05:26:08 +0800

    ?????

--- database/db.php
+++ database/db.php
@@ -4,7 +4,7 @@ $dbServername = "sql305.epizy.com";
 $dbUsername = "epiz_34076304";
 $dbPassword = "1QGEuAyZ0j17PXo";
 // //database
-// $dbName = "epiz_34076304_careerGuidance";
+$dbName = "epiz_34076304_careerGuidance";
 //   $dsn = "mysql: dbname= epiz_34076304_careerGuidance; host=sql305.epizy.com";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
